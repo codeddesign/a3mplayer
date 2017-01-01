3a122a2611c19cdc01c74ba94e6932d6f9ef9cbc
adelin <boovad@live.com>
2017-01-01 16:28:15 +0200

    Update config example

--- config.example.js
+++ config.example.js
@@ -1,3 +1,11 @@
 export default {
-
+    assets: [
+        {
+            name: 'player',
+            tag: 'script',
+            attributes: {
+                src: `http://a3mplayer.dev/player.js`
+            }
+        }
+    ]
 };
