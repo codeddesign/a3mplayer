802d4d2335619b7c308117a409e0f08e6d6b43c5
adelin <boovad@live.com>
2017-01-04 03:04:43 +0200

    Config updates

--- config.example.js
+++ config.example.js
@@ -1,11 +1,30 @@
 export default {
-    assets: [
-        {
-            name: 'player',
-            tag: 'script',
-            attributes: {
-                src: `http://a3mplayer.dev/player.js`
-            }
+    animator_fps: 60,
+
+    filler: {
+        client: 'ca-pub-1234567890123456',
+        slot: 1234567890,
+        pixels: -50
+    },
+
+    assets: [{
+        name: 'style',
+        tag: 'link',
+        attributes: {
+            rel: 'stylesheet',
+            href: `http://a3mplayer.dev/style.css`
         }
-    ]
+    }, {
+        name: 'player',
+        tag: 'script',
+        attributes: {
+            src: `http://a3mplayer.dev/player.js`
+        }
+    }, {
+        name: 'filler',
+        tag: 'script',
+        attributes: {
+            src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+        }
+    }]
 };
